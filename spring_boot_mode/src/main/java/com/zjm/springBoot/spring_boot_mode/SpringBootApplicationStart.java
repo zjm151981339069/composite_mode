b22f413f565e9e397cca90a80b79a8b180207d05
Administrator <1141266205@qq.com>
2018-05-28 09:40:40 +0800

    初始化

---
+++ spring_boot_mode/src/main/java/com/zjm/springBoot/spring_boot_mode/SpringBootApplicationStart.java
@@ -0,0 +1,18 @@
+package com.zjm.springBoot.spring_boot_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationStart {
+	public static void main(String[] args) {
+		System.err.println("****");
+		SpringApplication.run(SpringBootApplicationStart.class, args);
+		System.err.println("starting ****");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
