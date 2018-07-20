18687e38cdadc6e7a4e1159c21ab2d26c5727701
Administrator <1141266205@qq.com>
2018-07-20 15:49:23 +0800

    4444

--- spring_boot_mode/src/main/java/com/zjm/springBoot/spring_boot_mode/SpringBootApplicationStart.java
+++ spring_boot_mode/src/main/java/com/zjm/springBoot/spring_boot_mode/SpringBootApplicationStart.java
@@ -1,18 +1,10 @@
 package com.zjm.springBoot.spring_boot_mode;
 
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import com.zjm.springBoot.spring_boot_mode.kafka.KafkaSender;
 
 @SpringBootApplication
 public class SpringBootApplicationStart {
-   @Resource
-   private KafkaSender kafkaSender;
 	
 	
 	public static void main(String[] args) {
@@ -25,9 +17,4 @@ public class SpringBootApplicationStart {
 			e.printStackTrace();
 		}
 	}
-	 //然后每隔1分钟执行一次
-    @Scheduled(fixedRate = 1000)
-    public void testKafka() throws Exception {
-        kafkaSender.sendTest();
-    }
 }
